515e69cfc5cf289fba7ec627a0c27bcd75206e2f
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-15 20:15:30 +0000

    stage1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 rock = '''
     _______
 ---'   ____)
@@ -24,4 +26,20 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+# var ranking system for game  
+scissors > rock  
+rock > paper 
+paper > scissors 
+# message to player
+print(f" welcome to my game{rock}rock {paper}paper {scissors}scissors  \n \nPRESS:\n1 for rock\n2 for paper\n3 for scissors")
+# player input choice
+choice = input("What do you chose:")
+# make random gen for out come
+game_1 = (scissors, rock, paper)
+ = random.gitrandbit(game_1)
 
+
+if choice == True:
+
+
+  print("you win")
